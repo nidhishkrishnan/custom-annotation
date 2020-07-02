47dc592f65247f188ed288c224f566e522de4d88
nidhishkrishnan <nidhishkrishnan@gmail.com>
2020-07-02 22:34:35 +0200

    Initial Commit

---
+++ custom-annotation/src/test/java/com/customannotation/CustomAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
