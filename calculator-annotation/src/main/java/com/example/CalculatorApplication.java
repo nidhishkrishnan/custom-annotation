b37e860513c65dfb6c676c8ccd872fe6c30b9faa
nidhishkrishnan <nidhishkrishnan@gmail.com>
2020-07-16 11:19:02 +0200

    Adding @EnableCalculatorProcessor

--- calculator-annotation/src/main/java/com/example/CalculatorApplication.java
+++ calculator-annotation/src/main/java/com/example/CalculatorApplication.java
@@ -1,9 +1,11 @@
 package com.example;
 
+import com.example.calculator.annotations.EnableCalculatorProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCalculatorProcessor("com.example.services")
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
